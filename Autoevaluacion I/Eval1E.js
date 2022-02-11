0a64dee5b18f626de426fa64c943efa89c3d1c81
alpha-romeo <fenrique310@gmail.com>
2022-02-11 18:16:19 -0300

    Respuesta del punto 5

--- Autoevaluacion I/Eval1E.js
+++ Autoevaluacion I/Eval1E.js
@@ -1,4 +1,8 @@
 var a,b;
-a=prompt("Escribe la base:")
-b=promp("Escribe la altura:")
-alert("Área= "+(a*b/2);
+a=prompt("Escribe la base:");
+b=prompt("Escribe la altura:");
+alert("Área= "+a*b/2);
+/*lo que hice fue arreglar las comillas que faltaban en las lineas 2 y 3.
+Tambien puse la t que le faltaba en el prompt de la linea 3. Y saque el "("
+que sobraba en el alert*/
+/*Lo que hace este este programa es calcular un Area x*/ 
