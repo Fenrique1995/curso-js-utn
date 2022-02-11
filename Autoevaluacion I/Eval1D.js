03ce6158aabd626ea3b026003eb45f51fb158051
alpha-romeo <fenrique310@gmail.com>
2022-02-11 18:01:40 -0300

    Respuesta al punto 4

---
+++ Autoevaluacion I/Eval1D.js
@@ -0,0 +1,8 @@
+var a,b,c,d;
+a = parseInt(prompt("Por favor dame un numero", ""));
+b = a * 2;
+c = a * 3;
+d = a *4;
+alert ("Este es el doble de ese Numero: "+b+"\n"+
+	"Este es el triple de ese Numero: "+c+"\n"+
+	"Este es el cuatruple de ese Numero: "+d+".");
