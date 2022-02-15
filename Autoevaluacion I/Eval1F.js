dcd4706c470bc47c964256fc4a5a7c20da37eeb2
alpha-romeo <fenrique310@gmail.com>
2022-02-15 01:09:55 -0300

    Respuesta al punto 6

---
+++ Autoevaluacion I/Eval1F.js
@@ -0,0 +1,8 @@
+var a,b,c,d,e;
+a = parseInt(prompt("Ingresa el diametro de tu circulo",""));
+b = parseInt(prompt("Ingresa el radio de tu circulo",""));
+c = a * 3,1416;
+d = Math.pow(b,2);
+e = d * 3,1416;
+	alert("La longitud de tu circulo es: "+c+"\n"+
+		"Y su area es: "+e+".");
